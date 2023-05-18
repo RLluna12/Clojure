148c957bc0064e8079b95b652f15f7bbe15c522a
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2023-05-18 11:02:51 -0300

    Add files via upload

---
+++ aula dia 18 do 05.clj
@@ -0,0 +1,28 @@
+(println(str "4 tem tipo: "(type 4)))
+(println(str "9.99 tem tipo: "(type 9.99)))
+(println(str "a tem tipo: "(type \a)))
+(println(str "Hello turma tem tipo: "(type "hello turma...")))
+(println(str "true tem tipo: "(type true)))
+(println(str "false tem tipo: "(type false)))
+(println '(+ 2 3))
+(println ( eval '(+ 2 3) ) )
+
+(def A "Hello")
+(def B "Wolrd")
+(println (str A B))
+
+(def A 10)
+(def B 20)
+
+(println (+ A B))
+(println (+ A 2))
+(println (+ B 2))
+
+(println (str "A=" A))
+(println (str "B=" B))
+
+(def A 100)
+(def B 200)
+
+(println (str "A=" A))
+(println (str "B=" B))
